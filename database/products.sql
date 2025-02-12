24b457652501dc8aa42579a2020f8e63bfa0b4fb
Chris Katevas <katevaschris@outlook.com>
2025-02-12 23:39:33 +0200

    connect products -> categories

--- database/products.sql
+++ database/products.sql
@@ -3,5 +3,7 @@ CREATE TABLE products (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price DECIMAL(10,2) NOT NULL CHECK (price >= 0),
-    stock INT NOT NULL CHECK (stock >= 0)
+    stock INT NOT NULL CHECK (stock >= 0),
+    category_id INT REFERENCES categories(id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT NOW()
 );
