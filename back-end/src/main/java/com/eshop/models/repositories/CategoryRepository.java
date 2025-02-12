de14e943d995566bd4cdb4a3eecbf0bc33d43a9a
Chris Katevas <katevaschris@outlook.com>
2025-02-12 23:55:48 +0200

    Repositories

--- back-end/src/main/java/com/eshop/models/repositories/CategoryRepository.java
+++ back-end/src/main/java/com/eshop/models/repositories/CategoryRepository.java
@@ -1,11 +1,9 @@
 package com.eshop.models.repositories;
 
-
-import com.eshop.models.entities.Product;
+import com.eshop.models.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductsRepository extends JpaRepository<Product, Long>{
-	
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
